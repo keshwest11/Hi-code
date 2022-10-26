251037f788b95568a2aa3063f83bca1d06cc9952
keshwest11 <116770363+keshwest11@users.noreply.github.com>
2022-10-27 09:51:35 +1000

    Update Hicode.js

--- Hicode.js
+++ Hicode.js
@@ -1,5 +1,4 @@
-if (a == "gfeholly")
-    {"From the left... my first follower, sexy alien, professional cat lady and everyone's favourite girlfriend holly heart ❤"} 
-else if (a == "rachel__76")
-    {"It's fade grips affiliate Rachel 76! Ask her for a code to get a discount off your purchase!  Always a pleasure to have you here Rachel!"} 
-else {"Hello and welcome. I'm ET and I consensually abduct earthlings for experimentation. Give me a follow."}
+if (a == "TwitchName"){"Custom greeting for relevant user".}
+else if (a == "OtherTwitchName"){"Other custom greeting for relevant user".}
+else {"Generic greeting."}
+  
