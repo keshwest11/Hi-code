a748be846a68ea67d9d82736e4feb493f322449c
keshwest11 <116770363+keshwest11@users.noreply.github.com>
2022-10-27 10:26:17 +1000

    Update hicode.js

--- hicode.js
+++ hicode.js
@@ -2,4 +2,4 @@
 if (a == "TwitchName"){"Custom greeting for relevant user."}
 else if (a == "OtherTwitchName"){"Other custom greeting for relevant user."}
 else {"Generic greeting."}
-<\script>  
+</script>  
